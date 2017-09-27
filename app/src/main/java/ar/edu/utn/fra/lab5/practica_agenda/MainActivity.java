01fec1bbad8970eb8be88c8341fb25efc4598410
lkdml <lazaro2002@hotmail.com>
2017-09-27 18:00:02 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/utn/fra/lab5/practica_agenda/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.fra.lab5.practica_agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
